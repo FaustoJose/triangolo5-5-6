c323993018e17df761f88b66b47d3afcd66b4efa
Fausto Jose Tineo Mejia <2016-2155@est.itsc.edu.do>
2021-08-08 21:13:26 -0400

    Agregar archivos de proyecto.

---
+++ triangolo5,5,6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace triangolo5_5_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            triangulo("f",10);
+        }
+        public static void triangulo(string letra,int num)
+        {
+            int u = 0;
+            for (int i=0;i<=num-1;i++)
+            {
+                for (int k = 1;k<num-u; k++)
+                {
+                    
+
+                    Console.Write(letra);
+                }
+                Console.WriteLine(letra);
+                u = u + 1;
+            }
+
+        }
+    }
+}
